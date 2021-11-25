703f021a46230011e1c130cc02172e001431226a
Cheryl <Cheryl@LAPTOP-TI3OQHP8.mynetworksettings.com>
2021-11-24 20:00:05 -0500

    added subNum method

--- src/one/One.java
+++ src/one/One.java
@@ -4,5 +4,11 @@ public class One {
 public static void main(String[] args) {
 	
 	System.out.println("Practice github");
+	
+}
+
+public static void subNum(int num1, int num2) {
+	
+	System.out.println(num1+num2);
 }
 }
